b543a3a366d43681f9140c4f8f85db013a9460ce
Demon <hoaim2523002@gstudent.ctu.edu.vn>
2026-02-20 10:25:13 +0700

    Create GHA

--- src/test/java/com/samsung/s28utra/S28utraApplicationTests.java
+++ src/test/java/com/samsung/s28utra/S28utraApplicationTests.java
@@ -3,7 +3,7 @@ package com.samsung.s28utra;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class S28utraApplicationTests {
 
 	@Test
